ba6d88c9fbca54d98fbe9c254d668483da738d1a
Clara <clarafdezfraga@gmail.com>
2024-07-03 13:47:43 +0200

    Add component App to main

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+const App = (): React.ReactElement => {
+  return (
+    <div className="app">
+      <h1>Github Repositories</h1>
+    </div>
+  );
+};
+
+export default App;
