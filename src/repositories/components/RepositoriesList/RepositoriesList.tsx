ff947fd8809a9c4f7a5a644498c21e2cbce9044c
Clara <clarafdezfraga@gmail.com>
2024-07-05 19:00:47 +0200

    Add an emptylist image

--- src/repositories/components/RepositoriesList/RepositoriesList.tsx
+++ src/repositories/components/RepositoriesList/RepositoriesList.tsx
@@ -6,6 +6,16 @@ import "./RepositoriesList.scss";
 const RepositoriesList = (): React.ReactElement => {
   const { repositories } = useAppSelector((state) => state.repositoriesReducer);
 
+  if (repositories.length === 0) {
+    return (
+      <img
+        className="empty-list-image"
+        src="assets/search-holder.webp"
+        alt="Github mascot looking through binoculars at the building  in a supernatural landscape"
+      />
+    );
+  }
+
   return (
     <ul className="repositories">
       {repositories.map((repository: Repository) => (
