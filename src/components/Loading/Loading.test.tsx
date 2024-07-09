ffabe84f89b7d4a869311fe562d73b7c88f359a0
Clara <clarafdezfraga@gmail.com>
2024-07-09 11:58:26 +0200

    Fix Loading test

--- src/components/Loading/Loading.test.tsx
+++ src/components/Loading/Loading.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import Loading from "./Loading";
 
-describe("given a Loading component", () => {
-  it("should show a spinner ith the label 'Loading'", () => {
+describe("Given a Loading component", () => {
+  it("Should show a spinner with the label 'Loading'", () => {
     render(<Loading />);
 
     const loading = screen.getByLabelText("Loading");
